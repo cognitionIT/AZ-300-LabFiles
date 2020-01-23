b219d72e7de3ce23a1ebea39bc619d34d7d8c31d
Esther Barthel <cognitionIT@users.noreply.github.com>
2020-01-23 14:55:34 +0100

    Day 2

---
+++ Scripts/Day2.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module -Name Az
+
+Connect-AzAccount
+
+$myVNet2 = New-AzVirtualNetwork -ResourceGroupName az300RG -Location westeurope -Name myVnet2 -AddressPrefix 10.0.0.0/16
+
+Get-AzVirtualNetwork -Name myVnet2
+
+$mySubnet2 = Add-AzVirtualNetworkSubnetConfig -Name mySubnet2 -AddressPrefix 10.0.0.0/24 -VirtualNetwork $myVNet2
+
+Get-AzVirtualNetworkSubnetConfig -Name mySubnet2 -VirtualNetwork $myVNet2
+
+$mySubnet2 | Set-AzVirtualNetwork
+
+Get-AzNetworkInterface -Name myNic1 -ResourceGroupName az300RG
+
+$nic = Get-AzNetworkInterface -Name myNic1 -ResourceGroupName az300RG
+$nic.IpConfigurations[0].PrivateIpAllocationMethod = "Static"
+Set-AzNetworkInterface -NetworkInterface $nic
---
+++ Scripts/GettingStarted.ps1
@@ -0,0 +1,7 @@
+﻿#Azure AD PowerShell Module
+Install-Module AzureAD -AllowClobber #-Scope CurrentUser
+
+# Install Azure CLI
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+
+<# You can now run the Azure CLI with the az command from either Windows Command Prompt or PowerShell. PowerShell offers some tab completion features not available from Windows Command Prompt. To sign in, run the az login command.#>
